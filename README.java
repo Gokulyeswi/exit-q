c7c8fc68782604dc9de6dd241d8c143ffb28cf9c
Gokula Krishnan <gokulyeswanth@gmail.com>
2016-07-02 18:16:48 +0530

    finish

---
+++ README.java
@@ -0,0 +1,13 @@
+# exit-q
+import java.util.*;
+public class ExitwhenQ
+{
+public static void main(String ar[])throws Exception
+{
+String p=new String();
+Scanner sc=new Scanner(System.in);
+p=sc.next();
+if(p.equals("Q"))
+System.exit(0);
+}
+}
